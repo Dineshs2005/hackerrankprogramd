1825f0787ef6c1f86bee9dbd35a31d2691890e1b
Dineshs2005 <dinesh6383dh@gmail.com>
2025-05-15 21:31:40 +0530

    Create xoposi.java

---
+++ xoposi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class XOPosition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int row = (n - 1) / 3;
+        int col = (n - 1) % 3;
+        
+        System.out.println(row + " " + col);
+        
+        sc.close();
+    }
+}
